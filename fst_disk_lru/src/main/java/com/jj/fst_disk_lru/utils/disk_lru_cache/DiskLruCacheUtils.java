5903e2bb20fa1f4cded40a0629a5763195d7598f
admin <admin>
2020-03-23 17:58:17 +0800

    Jay 1.添加diskCacheAllFilesInZip功能

--- fst_disk_lru/src/main/java/com/jj/fst_disk_lru/utils/disk_lru_cache/DiskLruCacheUtils.java
+++ fst_disk_lru/src/main/java/com/jj/fst_disk_lru/utils/disk_lru_cache/DiskLruCacheUtils.java
@@ -6,18 +6,27 @@ import android.os.Looper;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.blankj.utilcode.util.ObjectUtils;
 import com.blankj.utilcode.util.Utils;
+import com.blankj.utilcode.util.ZipUtils;
 import com.jj.fst_disk_lru.utils.FSTUtils;
 import com.jj.fst_disk_lru.utils.IOUtils;
 import com.jj.fst_disk_lru.utils.Md5Utils;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.util.List;
 
 ;
 
@@ -257,6 +266,124 @@ public class DiskLruCacheUtils {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 将zipFile文件解压,并将解压文件都磁盘缓存
+     */
+    public synchronized boolean diskCacheAllFilesInZip(String zipFileUrl, InputStream inputStream) throws RuntimeException {
+        Log.d(TAG, "diskCacheBookZip zipFileUrl=" + zipFileUrl);
+
+        int lastIndexOfl = zipFileUrl.lastIndexOf("/");
+        if (lastIndexOfl <= 0) {
+            Log.e(TAG, "ZipUrl 异常");
+            return false;
+        }
+
+        if (Looper.getMainLooper() == Looper.myLooper()) {
+            throw new RuntimeException("不能在主线程执行IO操作");
+        }
+
+        List<File> unzipFileList;
+        String destDirPath = Utils.getApp().getFilesDir().getAbsolutePath() + File.separator + DISK_LRU_CACHE_DIR;
+        try {
+            //缓存bookZip包,耗时所在eg:<<英语测试卷>>40s
+            DiskLruCacheUtils.getInstance().writeFile(zipFileUrl, inputStream);
+            Log.d(TAG, "diskCacheAllFilesInZip Succeed ");
+
+            //将bookZip包 解压到 Constants.DISK_LRU_CACHE_DIR
+            String zipFileUrlKey = Md5Utils.encode(zipFileUrl);
+            String zipFilePath = destDirPath + File.separator + zipFileUrlKey + ".0";
+            unzipFileList = ZipUtils.unzipFile(zipFilePath, destDirPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        //以下都不耗时,如不失败,很快就成功了
+        Log.d(TAG, "diskCacheBookZip unzipFile Succeed ");
+
+        if (ObjectUtils.isEmpty(unzipFileList)) {
+            Log.w(TAG, "Zip包内无文件");
+            return true;
+        }
+
+        //将解压出来的文件重命名成 "key".0格式
+        String zipFileOssDir = zipFileUrl.substring(0, lastIndexOfl);
+        Log.d(TAG, "lastIndexOfl=" + lastIndexOfl + "? zipFileOssDir=" + zipFileOssDir);
+
+        File journalFile = new File(destDirPath, "journal");
+        Writer journalWriter = null;
+        try {
+
+            journalWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(journalFile, true), Charset.forName("US-ASCII")));
+            for (File unzipFile : unzipFileList) {
+                String unzipFileName = unzipFile.getName();
+                Log.d(TAG, "unzipFileName=" + unzipFileName);
+                String encodedKey = Md5Utils.encode(zipFileOssDir + "/" + unzipFileName);
+                File destFile = new File(destDirPath + "/" + encodedKey + ".0");
+                boolean renameTo = unzipFile.renameTo(destFile);
+                Log.d(TAG, "encodedKey=" + encodedKey + "? renameTo=" + renameTo);
+
+                //写journal
+                journalWriter.write("DIRTY" + ' ' + encodedKey + '\n');
+                journalWriter.flush();
+                File cleanFile = new File(destDirPath, encodedKey + "." + 0);
+                journalWriter.write("CLEAN" + ' ' + encodedKey + ' ' + cleanFile.length() + '\n');
+                journalWriter.flush();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.e(TAG, "DiskLruCache尚未初始化 call getInstance() first!");
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (journalWriter != null) {
+                try {
+                    journalWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //手动关闭,等下次重新实例化,以上变更生效
+        DiskLruCacheUtils.getInstance().close();
+
+        return true;
+    }
+
+    private void writeFile(String url, InputStream inputStream) throws IOException {
+        Log.d(TAG, "writeFile url=" + url);
+
+        String key = Md5Utils.encode(url);
+
+        OutputStream outputStream = null;
+        try {
+
+            DiskLruCache.Editor edit = mDiskLruCache.edit(key);
+            if (edit != null) {
+                outputStream = edit.newOutputStream(0);
+                byte[] bytes = new byte[1024];
+                int len;
+                while ((len = inputStream.read(bytes)) > 0) {
+                    outputStream.write(bytes, 0, len);
+                }
+                outputStream.flush();
+                edit.commit();
+                mDiskLruCache.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            IOUtils.safeClose(outputStream);
+            IOUtils.safeClose(inputStream);
+        }
+
     }
 
     public void close() {
